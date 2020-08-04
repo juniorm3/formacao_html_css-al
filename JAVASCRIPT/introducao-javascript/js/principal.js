874714a74b86efb0d7111129c0a2a17459de3014
Mario Junior <juniorm3@msn.com>
2020-08-03 22:18:33 -0300

    Arrays, Loop e Estilos

--- JAVASCRIPT/introducao-javascript/js/principal.js
+++ JAVASCRIPT/introducao-javascript/js/principal.js
@@ -1,35 +1,52 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
-var alturaEhValida = true;
-var pesoEhValido = true;
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-if(peso <= 0 || peso > 1000){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
+    if(peso <= 0 || peso > 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura inválida!");
+    
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(pesoEhValido && alturaEhValida){    
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    
+    } else {
+        tdImc.textContent = "Altura e/ou inválidos!";
+    }
+
+
+    console.log(paciente);
 }
 
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida!");
+//var paciente = document.querySelector("#primeiro-paciente");
 
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
 
-if(pesoEhValido && alturaEhValida){    
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-
-} else {
-    tdImc.textContent = "Altura e/ou inválidos!";
-}
 
