e8e1f98a4609535634d85d24a6c41ea5cc69282a
Mario Junior <juniorm3@msn.com>
2020-08-25 21:19:54 -0300

    Filtrando uma tabela

--- JAVASCRIPT/introducao-javascript/js/fitlra.js
+++ JAVASCRIPT/introducao-javascript/js/fitlra.js
@@ -6,7 +6,19 @@ campoFiltro.addEventListener("input", function(){
     if(this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++){
             var paciente = pacientes[i];
-            paciente.classList.add("invisivel");
+
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            var expressao = new RegExp(this.value, "i");
+
+            if(expressao.test(nome)){
+                paciente.classList.remove("invisivel");
+
+            } else {
+                paciente.classList.add("invisivel");
+            }
+
         }
     } else {
         for (var i = 0; i < pacientes.length; i++) {
